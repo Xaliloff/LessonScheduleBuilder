be0b2274d05f93e864431a6d244ff6106d79ce93
Kamran Khalilov <Kamran_Khalilov@epam.com>
2021-03-08 02:02:21 +0300

    cmoot

--- LessonsScheduleBuilder.Data/Migrations/20210307224310_initial.cs
+++ LessonsScheduleBuilder.Data/Migrations/20210307224310_initial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace LessonsScheduleBuilder.Data.Migrations
 {
@@ -6,10 +7,164 @@ namespace LessonsScheduleBuilder.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grade = table.Column<short>(type: "smallint", nullable: false),
+                    GroupSuffix = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LessonTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LessonTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GroupId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleLessons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DayOfTheWeek = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    LessonTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    LessonTypeId = table.Column<int>(type: "int", nullable: false),
+                    GroupId = table.Column<int>(type: "int", nullable: false),
+                    SelectedTeacherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleLessons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleLessons_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ScheduleLessons_LessonTypes_LessonTypeId",
+                        column: x => x.LessonTypeId,
+                        principalTable: "LessonTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduleLessons_Users_SelectedTeacherId",
+                        column: x => x.SelectedTeacherId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeacherSpecializations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherId = table.Column<int>(type: "int", nullable: false),
+                    LessonTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeacherSpecializations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeacherSpecializations_LessonTypes_LessonTypeId",
+                        column: x => x.LessonTypeId,
+                        principalTable: "LessonTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeacherSpecializations_Users_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleLessons_GroupId",
+                table: "ScheduleLessons",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleLessons_LessonTypeId",
+                table: "ScheduleLessons",
+                column: "LessonTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleLessons_SelectedTeacherId",
+                table: "ScheduleLessons",
+                column: "SelectedTeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherSpecializations_LessonTypeId",
+                table: "TeacherSpecializations",
+                column: "LessonTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeacherSpecializations_TeacherId",
+                table: "TeacherSpecializations",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GroupId",
+                table: "Users",
+                column: "GroupId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
-        { 
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleLessons");
+
+            migrationBuilder.DropTable(
+                name: "TeacherSpecializations");
+
+            migrationBuilder.DropTable(
+                name: "LessonTypes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
         }
     }
 }
